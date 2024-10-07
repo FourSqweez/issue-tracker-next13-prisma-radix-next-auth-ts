bf6bbea68555b9a3436d9d7b668bb69dc94d9d83
FourSqweez <jeerasak.kr@chomchob.com>
2024-10-07 15:21:56 +0700

    Add : build a reusable styled link component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,10 @@
 import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
+import Link from "../components/Link";
 import React from "react";
 import IssueStatusBadge from "../components/IssueStatusBadge";
 import delay from "delay";
 import IssueActions from "./IssueActions";
-import Link from "next/link";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
